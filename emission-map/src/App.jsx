32109db728e8e9913316d11bd2c8af630ef6ad4e
sMorrison26 <morris10@rpi.edu>
2025-02-15 13:46:46 -0500

    Centering and controlling the map

--- emission-map/src/App.jsx
+++ emission-map/src/App.jsx
@@ -3,9 +3,13 @@ import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "./App.css";
 
+const INITIAL_CENTER = [-73.935242, 40.730610]
+const INITIAL_ZOOM = 10.12
 
 function App() {
 	const [count, setCount] = useState(0);
+  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [zoom, setZoom] = useState(INITIAL_ZOOM)
 
   const mapRef = useRef()
   const mapContainerRef = useRef()
@@ -14,6 +18,8 @@ function App() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuZ2IyNCIsImEiOiJjbTc2amR0NXIwdXphMmxwcnZtanprZXZzIn0.heEc3MDcr2E2grB7VXRKgw'
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
+      center: center,
+      zoom: zoom 
     });
 
     return () => {

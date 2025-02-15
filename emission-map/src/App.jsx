6cdd0ecb5544ad0f03e915cf33151078f5b59d1e
sMorrison26 <morris10@rpi.edu>
2025-02-15 17:41:30 -0500

    collect coordinates from user selection on map searchbox

--- emission-map/src/App.jsx
+++ emission-map/src/App.jsx
@@ -1,66 +1,96 @@
 import { useState, useRef, useEffect } from "react";
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { SearchBox } from "@mapbox/search-js-react";
 import SearchBar from "./components/searchbar";
 import "./App.css";
 
-const INITIAL_CENTER = [-73.935242, 40.730610]
-const INITIAL_ZOOM = 10.12
+const INITIAL_CENTER = [-73.935242, 40.73061];
+const INITIAL_ZOOM = 10.12;
 
 function App() {
 	const [count, setCount] = useState(0);
-  const [center, setCenter] = useState(INITIAL_CENTER)
-  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+	const [center, setCenter] = useState(INITIAL_CENTER);
+	const [zoom, setZoom] = useState(INITIAL_ZOOM);
+  const [inputValue, setInputValue] = useState("");
+  const [endCoords, setEndCoords] = useState();
 
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
+	const mapRef = useRef();
+	const mapContainerRef = useRef();
 
-  useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuZ2IyNCIsImEiOiJjbTc2amR0NXIwdXphMmxwcnZtanprZXZzIn0.heEc3MDcr2E2grB7VXRKgw'
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      center: center,
-      zoom: zoom,
-      style: 'mapbox://styles/mapbox/streets-v11',
-    });
+	useEffect(() => {
+		mapboxgl.accessToken = "pk.eyJ1IjoiZGFuZ2IyNCIsImEiOiJjbTc2amR0NXIwdXphMmxwcnZtanprZXZzIn0.heEc3MDcr2E2grB7VXRKgw";
+		mapRef.current = new mapboxgl.Map({
+			container: mapContainerRef.current,
+			center: center,
+			zoom: zoom,
+			style: "mapbox://styles/mapbox/streets-v11",
+		});
 
-    mapRef.current.on('move', () => {
-      // get the current center coordinates and zoom level from the map
-      const mapCenter = mapRef.current.getCenter()
-      const mapZoom = mapRef.current.getZoom()
+		// const marker = new mapboxgl.Marker() // initialize a new marker
+		//   .setLngLat([-73.935242, 40.730610]) // Marker [lng, lat] coordinates
+		//   .addTo(mapRef); // Add the marker to the map
 
-      // update state
-      setCenter([ mapCenter.lng, mapCenter.lat ])
-      setZoom(mapZoom)
-    })
+		mapRef.current.on("move", () => {
+			// get the current center coordinates and zoom level from the map
+			const mapCenter = mapRef.current.getCenter();
+			const mapZoom = mapRef.current.getZoom();
 
-    return () => {
-      mapRef.current.remove()
-    }
-  }, [])
+			// update state
+			setCenter([mapCenter.lng, mapCenter.lat]);
+			setZoom(mapZoom);
+		});
 
-  const handleReset = () => {
-    mapRef.current.flyTo({
-      center: INITIAL_CENTER,
-      zoom: INITIAL_ZOOM
-    })
+		return () => {
+			mapRef.current.remove();
+		};
+	}, []);
+
+	const handleReset = () => {
+		mapRef.current.flyTo({
+			center: INITIAL_CENTER,
+			zoom: INITIAL_ZOOM,
+		});
+	};
+
+  //collect the coordinates once the user clicks on their choice from the search bar
+  const handleRetrieve = (data) => {
+    // console.log("data",data);
+    // console.log("coords:",data.features[0].geometry.coordinates)
+    setEndCoords(data.features[0].geometry.coordinates)
   }
-  
 
-	return (
-		<>
-      <SearchBar />
+  return (
+    <>
       <div className="sidebar">
-        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
       </div>
-      <button className='reset-button' onClick={handleReset}>
+      <button className="reset-button" onClick={handleReset}>
         Reset
       </button>
-      
-			<div id="map-container" ref={mapContainerRef} />
-      
-		</>
-	);
+      <div className="search-box-container">
+        <SearchBox
+          accessToken="pk.eyJ1IjoiZGFuZ2IyNCIsImEiOiJjbTc2amR0NXIwdXphMmxwcnZtanprZXZzIn0.heEc3MDcr2E2grB7VXRKgw"
+          map={mapRef.current}
+          mapboxgl={mapboxgl}
+          value={inputValue}
+          onChange={(d) => {
+            setInputValue(d);
+          }}
+          options={{
+            language: "en",
+            country: "US",
+          }}
+          onRetrieve={handleRetrieve}
+          marker
+        />
+      </div>
+      <div id="map-container" ref={mapContainerRef} />
+    </>
+  );
 }
+
+
 
 export default App;
--- emission-map/src/components/searchbar.jsx
+++ emission-map/src/components/searchbar.jsx
@@ -1,15 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 import "./searchbar.css"
 
+const INITIAL_CENTER = [-73.935242, 40.730610]
+const INITIAL_ZOOM = 10.12
+
 export default function SearchBar(){
+
+  //api: https://api.mapbox.com/geocoding/v5
+  const [searchText, setSearchText] = useState('')
+
+  const handleSearchText = (text) => {
+    // console.log('Before: ',text);
+    setSearchText(encodeURI(text));
+    // console.log('After: ',searchText);
+  }
+
+  // const handleSearch = async (event) => {
+  //   if (event.key === 'Enter') {
+  //     console.log('Enter key pressed');
+  //     console.log('SearchText: ', searchText);
+  //     // Add your logic here to handle the search
+  //     try {
+  //       const apiToken = 'pk.eyJ1IjoiZGFuZ2IyNCIsImEiOiJjbTc2amR0NXIwdXphMmxwcnZtanprZXZzIn0.heEc3MDcr2E2grB7VXRKgw'
+  //       const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?proximity=${INITIAL_CENTER[0]},${INITIAL_CENTER[1]}&bbox=-122.30937,37.84214,-122.23715,37.89838&access_token=${apiToken}`)
+  //     } catch {
+
+  //     }
+
+  //   }
+  // }
+
   return (
-    <div className="searchbar">
-      <div className="searchbar-input-container">
-        <FontAwesomeIcon icon={faSearch} className="search-icon" />
-        <input type="text" placeholder='Search...'/>
-      </div>
-    </div>
+    // <div className="searchbar">
+    //   <div className="searchbar-input-container">
+    //     <FontAwesomeIcon icon={faSearch} className="search-icon" />
+    //     <input 
+    //       type="text" 
+    //       placeholder='Search...' 
+    //       onChange={(e) => handleSearchText(e.target.value) }
+    //       onKeyDown={handleSearch}
+    //     />
+    //   </div>
+    // </div>
+    <></>
   )
 }

635b280d498a70c860d522dfcd373c89bf6d8341
sMorrison26 <morris10@rpi.edu>
2025-02-15 13:52:17 -0500

    Adding coordinates and reset button

--- emission-map/src/App.jsx
+++ emission-map/src/App.jsx
@@ -19,16 +19,41 @@ function App() {
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       center: center,
-      zoom: zoom 
+      zoom: zoom,
+      style: 'mapbox://styles/mapbox/streets-v11',
     });
+
+    mapRef.current.on('move', () => {
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter()
+      const mapZoom = mapRef.current.getZoom()
+
+      // update state
+      setCenter([ mapCenter.lng, mapCenter.lat ])
+      setZoom(mapZoom)
+    })
 
     return () => {
       mapRef.current.remove()
     }
   }, [])
 
+  const handleReset = () => {
+    mapRef.current.flyTo({
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM
+    })
+  }
+  
+
 	return (
 		<>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
+      <button className='reset-button' onClick={handleReset}>
+        Reset
+      </button>
 			<div id="map-container" ref={mapContainerRef} />
 		</>
 	);

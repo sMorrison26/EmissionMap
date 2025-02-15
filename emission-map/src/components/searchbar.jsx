f5e4e4225bb279d414419304e7d1300f329d2f20
sMorrison26 <morris10@rpi.edu>
2025-02-15 14:32:15 -0500

    Search bar styling

--- emission-map/src/components/searchbar.jsx
+++ emission-map/src/components/searchbar.jsx
@@ -1,17 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenNib } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 import "./searchbar.css"
-
 
 export default function SearchBar(){
   return (
     <div className="searchbar">
-      {/* <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> */}
-      <FontAwesomeIcon icon={faPenNib} />
-      <FontAwesomeIcon icon={faEnvelope} />
-      <input type="text" placeholder='Search...'/>
+      <div className="searchbar-input-container">
+        <FontAwesomeIcon icon={faSearch} className="search-icon" />
+        <input type="text" placeholder='Search...'/>
+      </div>
     </div>
   )
 }
